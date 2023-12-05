caefe97c37543bb9f19621fffde9acab96391a1f
Reshka <reshetnyuk79@gmail.com>
2023-12-05 20:11:47 +0200

    done

---
+++ README.md
@@ -0,0 +1,20 @@
+# goit-node-hw-01
+
+Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+Посилання: https://ibb.co/VSM7pcy
+
+
+Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+Посилання: https://ibb.co/8YnSY3v
+
+
+Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+Посилання: https://ibb.co/BCbjhGJ
+
+
+Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+node index.js --action="remove" --id 4e7250a9-824b-4a8a-b9c2-f4f48e6ebe51
+Посилання: https://ibb.co/PhRN2dr
---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const { randomUUID } = require("crypto");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+
+  const contact = data.find((contact) => contact.id === contactId) || null;
+
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const data = await listContacts();
+
+  const index = data.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = data.splice(index, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+
+  return result;
+}
+
+async function addContact({ name, email, phone }) {
+  const data = await listContacts();
+
+  const newContact = {
+    id: randomUUID(),
+    name,
+    email,
+    phone,
+  };
+
+  data.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
